5b8e7133e4ca9244d7f4c2b7e924f8f8991ec0a1
manickyoj <hotshot354@gmail.com>
2015-09-25 11:45:08 -0400

    Added space gun sketch and a python script for sound-frequency to OCR setting conversions

---
+++ hz_calc.py
@@ -0,0 +1,27 @@
+CLOCK_FREQ = 16000000
+PRESCALER = 8
+OCR0A = 99
+LOOKUPS = 100
+
+# What freq should the arduino be outputting based on current settings
+timer_freq = CLOCK_FREQ / PRESCALER
+interrupt_frequency = timer_freq / (OCR0A + 1)
+sound_frequency = interrupt_frequency / LOOKUPS
+
+print("Sound frequency: " + str(sound_frequency))
+
+
+# # How should we set 
+# DESIRED_SOUND_FREQ = 200
+# desired_interrupt_freq = LOOKUPS * sound_frequency
+# desired_OCR0A = (timer_freq / desired_interrupt_freq) - 1
+
+# print("OCR0A setting: " + str(desired_OCR0A))
+
+TIMER_FREQ = 2000000
+LOOKUPS = 100
+desired_freq = 200
+
+desired_interrupt_freq = LOOKUPS * desired_freq
+desired_OCR0A = (TIMER_FREQ / desired_interrupt_freq) - 1
+print(desired_OCR0A)
